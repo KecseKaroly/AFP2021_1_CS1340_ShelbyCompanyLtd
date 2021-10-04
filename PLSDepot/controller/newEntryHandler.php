081fce68ec4a6060d2312fe177f10cb81683f590
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-10-04 21:03:46 +0200

    Update newEntryHandler.php

--- PLSDepot/controller/newEntryHandler.php
+++ PLSDepot/controller/newEntryHandler.php
@@ -14,26 +14,19 @@
                         else{
                                 echo "ERROR: Could not execute $query. " . mysqli_error($con);
                             }
-        $query="insert into megbizas(megnevezes, suly, ar) values ('".$_POST['csomaginfo_megnevezes']."','"
-                .$_POST['csomaginfo_suly']."','".$_POST['csomaginfo_ar']."')";
+        $query="insert into megbizas(megnevezes, suly, ar, partnercegid) values ('".$_POST['csomaginfo_megnevezes']."','"
+                .$_POST['csomaginfo_suly']."','".$_POST['csomaginfo_ar']."','".$_POST['partnercegek_nev']."')";
                 if(mysqli_query($con, $query))
                         echo "Records added successfully.";
                         else{
                                 echo "ERROR: Could not execute $query. " . mysqli_error($con);
                             }
-        $query="insert into csomaginfo(megnevezes, suly, ar) values ('".$_POST['csomaginfo_megnevezes']."','"
-                .$_POST['csomaginfo_suly']."','".$_POST['csomaginfo_ar']."')";
-                if(mysqli_query($con, $query))
-                        echo "Records added successfully.";
-                        else{
-                                echo "ERROR: Could not execute $query. " . mysqli_error($con);
-                            }
-                            
+
         
 
 
         mysqli_close($con);
-        echo '<meta http-equiv="refresh" content="0; URL=../view/menu.php">';
+        #echo '<meta http-equiv="refresh" content="0; URL=../view/menu.php">';
 
             
     }
