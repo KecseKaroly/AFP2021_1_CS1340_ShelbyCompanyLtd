db6d1e4507cc6ac203383e08fa432536cfb23879
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-10-05 08:55:34 +0200

    Update newEntryHandler.php

--- PLSDepot/controller/newEntryHandler.php
+++ PLSDepot/controller/newEntryHandler.php
@@ -7,15 +7,20 @@
         
         $dbname="pls";
         $con=connect("root","", $dbname);
-        
-        $query="insert into cimzett(nev, lakcim, telefonszam, email) values ('".$_POST['cimzett_nev']."' ,'" .$_POST['cimzett_lakcim']."' ,'" .$_POST['cimzett_tel']."' ,'" .$_POST['cimzett_email']."')";
+        $query="select count(id) as total from cimzett where  nev = '".$_POST['cimzett_nev']."' and lakcim = '" .$_POST['cimzett_lakcim']."'";
+        $res=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
+        list($result)=mysqli_fetch_row($res);
+        if ($result == 0) {
+            $query="insert into cimzett(nev, lakcim, telefonszam, email) values ('".$_POST['cimzett_nev']."' ,'" .$_POST['cimzett_lakcim']."' ,'" .$_POST['cimzett_tel']."' ,'" .$_POST['cimzett_email']."')";
                 if(mysqli_query($con, $query))
                         echo "Records added successfully.";
                         else{
                                 echo "ERROR: Could not execute $query. " . mysqli_error($con);
                             }
-        $query="insert into megbizas(megnevezes, suly, ar, partnercegid) values ('".$_POST['csomaginfo_megnevezes']."','"
-                .$_POST['csomaginfo_suly']."','".$_POST['csomaginfo_ar']."','".$_POST['partnercegek_nev']."')";
+        }
+        
+        $query="insert into megbizas(megnevezes, suly, ar, feladas_datum, cimzettid ,partnercegid) values ('".$_POST['csomaginfo_megnevezes']."','".$_POST['csomaginfo_suly']."','".$_POST['csomaginfo_ar']."', CURRENT_TIMESTAMP,
+        (select id from cimzett where  nev = '".$_POST['cimzett_nev']."' and lakcim = '" .$_POST['cimzett_lakcim']."'), (select id from partnercegek where  nev = '".$_POST['partnercegek_nev']."'))";
                 if(mysqli_query($con, $query))
                         echo "Records added successfully.";
                         else{
