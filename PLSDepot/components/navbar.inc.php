12c1f4fc1a8e5fd8328a20309bcf2ea8125480f8
FeNToNelele <91262730+FeNToNelele@users.noreply.github.com>
2021-10-05 20:05:00 +0000

    Navigációs csík implementálva

---
+++ PLSDepot/components/navbar.inc.php
@@ -0,0 +1,32 @@
+<head>
+  <nav class="navbar navbar-fixed-top navbar-expand-lg bg-light">
+    <img src="../img/PLS_logo.png" alt="" class="img-fluid" style="width: 20%;">
+    <div class="container">
+      <div class="navbar">
+        <ul class="navbar-nav d-flex flex-row">
+          <div class="row">
+
+            <!--Ha be van jelentkezve-->
+            <div class="col">
+              <img src="../img/megbizasok-menu.png" alt="Megbízások" style="width: 90%;">
+            </div>
+
+            <!--Ha rendelésfelvevő-->
+            <div class="col">
+              <img src="../img/uj_megbizas-menu.png" alt="Új megbízás" width="80%">
+            </div>
+
+            <!--Ha admin-->
+            <div class="col">
+              <img src="../img/felhasznalok_kezelese-menu.png" alt="Felhasználók" width="80%">
+            </div>
+
+
+          </div>
+        </ul>
+      </div>
+      <!--Kijelentkezés-->
+      <img src="../img/kijelentkezes-menu.png" alt="Kijelentkezés" width="15%">
+    </div>
+  </nav>
+</head>
