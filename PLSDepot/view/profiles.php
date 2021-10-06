c297aa008e42ba98f7a091f5ad643fd14405a49d
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-10-06 10:46:00 +0200

    Felhasználók adatainak kilistázása
    Felhasználók felhasználónevét, jogosultságát illetve teljes nevét kilistázza. Lehetőség van a felsorolt adatok módosítására, valamint a felhasználó törlésére.

---
+++ PLSDepot/view/profiles.php
@@ -0,0 +1,57 @@
+<?php
+    require('../components/init.inc.php');
+    require ('../controller/database.php');
+    $dbname="pls";
+    $con=connect("root","",$dbname);
+    session_start();
+    $query="select jog from alkalmazott where felhasznalonev='".$_SESSION['user']."'";
+    $res=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
+    list($aut)=mysqli_fetch_row($res);
+    if((empty($_SESSION['userLogin']) || $_SESSION['userLogin'] == '') || $aut!="admin"){
+        echo '<meta http-equiv="refresh" content="0; URL=index.php">';
+    }
+?>
+
+
+<?php
+    if(true) {
+        echo '<div class="container-md">
+        <table class="table table-light" style="text-shadow: none;">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Teljes név</th>
+                <th scope="col">Felhasználónév</th>
+                <th scope="col">Jog</th>
+                <th scope="col">Módosítás</th>
+                <th scope="col">Törlés</th>
+              </tr>
+            </thead>
+            <tbody>';
+    
+
+    $sql = "SELECT * FROM alkalmazott WHERE jog != 'admin'";
+
+    $con=mysqli_connect('localhost',"root","", "pls",3306);
+    $result=mysqli_query($con,$sql) or die ("Nem sikerült ".$sql);
+        while(list($id,$teljesnev,$felhasznalonev,$jelszo,$jog)=mysqli_fetch_row($result)) {
+        
+            echo "<tr>";
+                echo '<form name="deletemodifyUser" method="post" action="deletemodifyUser.php" enctype=multipart/form-data>';
+                echo "<th scope='row'>".$id."</th>";
+                echo '<input type="hidden" name="id" value='.$id.'>';
+                echo "<td>".$teljesnev."</td>";
+                echo '<input type="hidden" name="teljesnev" value="'.$teljesnev.'">';
+                echo "<td>".$felhasznalonev."</td>";
+                echo '<input type="hidden" name="felhasznalonev" value="'.$felhasznalonev.'">';
+                echo "<td>".$jog."</td>";
+                echo '<input type="hidden" name="jog" value="'.$jog.'">';
+                echo "<td><button type='submit' name='modifyUser' class='btn btn-warning btn-lg'>Módosítás</button></td>";
+                echo "<td><button type='submit' name='deleteUser' class='btn btn-danger btn-lg'>Törlés</button></td>";
+                echo '</form>';
+            echo "</tr>";
+        }
+    }
+    ?> 
+
+</div>
