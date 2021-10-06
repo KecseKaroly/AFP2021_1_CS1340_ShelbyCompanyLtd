009ea2473876c47e6b1cba290e13831572051233
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-10-06 10:52:33 +0200

    Felhasználó módosítás SQL
    Felhasználói adatok módosítását végző SQL utasítások.

---
+++ PLSDepot/controller/uploadModifiedUser.php
@@ -0,0 +1,28 @@
+<?php
+    require('../components/init.inc.php');
+    require ('../controller/database.php');
+?>
+<?php
+if(isset($_POST['uploadModifiedUser']))
+{
+
+    $id = (int)$_POST['id'];
+    $teljesnev = $_POST['teljesnev'];
+    $felhasznalonev = $_POST['felhasznalonev'];
+    $jelszo = $_POST['jelszo'];
+    $jog = $_POST['jog'];
+    
+    $sql='UPDATE alkalmazott SET teljesnev="'.$teljesnev.'", felhasznalonev="'.$felhasznalonev.'", jog="'.$jog.'", jelszo=md5("'.$jelszo.'") WHERE id='.$id.';';
+    if($jelszo != null)
+        $sql='UPDATE alkalmazott SET jelszo = md5("'.$jelszo.'") WHERE id='.$id.';';
+    
+
+    $con=connect("root","", "pls");
+    $result=mysqli_query($con,$sql) or die ("Nem sikerült ".$sql);
+
+    if($result)
+    {
+        echo '<h2 style="text-align: center;">A kiválasztott felhasználó sikeresen módosítva.</h2>';
+    } else echo '<h2 style="text-align: center;">Hiba a lekérdezés lefutásakor.</h2>';
+}
+?>
