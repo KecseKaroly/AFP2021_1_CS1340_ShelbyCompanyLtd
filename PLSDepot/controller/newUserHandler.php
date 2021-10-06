9f9cfe93cfbc953ea5224a284165d64b1d8fa916
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2021-10-06 10:51:30 +0200

    Új felhasználó hozzáadás SQL
    Új profilok hozzáadását elvégző SQL műveleteket tartalmazza

---
+++ PLSDepot/controller/newUserHandler.php
@@ -0,0 +1,29 @@
+<?php
+    if(isset($_POST["newuser"]))  
+    {
+        require "database.php";
+        
+        
+        
+        $dbname="pls";
+        $con=connect("root","", $dbname);
+        
+        $query="insert into alkalmazott(teljesnev, felhasznalonev, jelszo, jog) values ('".$_POST['teljesnev']."' ,'" .$_POST['felhasznalonev']."' ,'" .md5($_POST['jelszo'])."' ,'" .$_POST['jog']."')";
+                if(mysqli_query($con, $query))
+                        echo "Records added successfully.";
+                        else{
+                                echo "ERROR: Could not execute $query. " . mysqli_error($con);
+                            }
+
+        
+
+
+        mysqli_close($con);
+        #echo '<meta http-equiv="refresh" content="0; URL=../view/menu.php">';
+
+            
+    }
+       
+
+    
+?>
