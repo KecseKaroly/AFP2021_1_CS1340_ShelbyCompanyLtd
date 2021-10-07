b470187724e0558ef6645b235de5854f7054684f
FeNToNelele <91262730+FeNToNelele@users.noreply.github.com>
2021-10-07 15:04:48 +0200

    Átirányító script navbarhoz

---
+++ PLSDepot/js/redirect.js
@@ -0,0 +1,20 @@
+function Redirect(panel) {
+    switch(panel) {
+        case 0:
+          // menü
+          window.location.replace("menu.php");
+          break;
+        case 1:
+          // megbízások
+          window.location.replace("listorders.php");
+          break;
+        case 2:
+            // új megbízás
+            window.location.replace("newentryform.php");
+          break;
+        case 3:
+            // felhasználók
+            window.location.replace("profiles.php");
+          break;
+      }
+}
