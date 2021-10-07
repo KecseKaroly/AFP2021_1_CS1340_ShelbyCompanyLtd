f911b9db507d40c33716078b860a4c203d027a80
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-10-07 14:32:15 +0200

    Autentikációs ellenőrzés globálisan felírva
    Tesztelni kell, mert nem biztos, hogy mindenütt átírtam jól

--- PLSDepot/controller/loginHandler.php
+++ PLSDepot/controller/loginHandler.php
@@ -11,6 +11,9 @@
 		if($passwd==md5($_POST['password']) && $username==($_POST['username']))
 		{
                     session_start();
+                    $query="select jog from alkalmazott where felhasznalonev='".$_SESSION['user']."'";
+                    $res=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
+                    list($_SESSION['aut'])=mysqli_fetch_row($res);
                     $_SESSION["user"]=$username;
                     $cookie_name = $username;
                     $a=getenv("REMOTE_ADDR"); 
