314f88e7a964b8b61ec2cf80801a95b82c1acf5f
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-10-12 20:49:02 +0200

    Végleges verzió

--- PLSDepot/controller/loginHandler.php
+++ PLSDepot/controller/loginHandler.php
@@ -1,32 +1,29 @@
 <?php
-            require 'database.php';
-            if(isset($_POST["login"])){
-                $dbname="pls";
-                $con=connect("root","", $dbname);
-                
+require 'database.php';
+if(isset($_POST["login"]))
+	{
+		$dbname="pls";
+        	$con=connect("root","", $dbname);
 		$query="select felhasznalonev,jelszo,jog from alkalmazott where felhasznalonev='".$_POST['username']."'";
 		$result=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
-                
-        list($username,$passwd,$aut)=mysqli_fetch_row($result);
+        	list($username,$passwd,$aut)=mysqli_fetch_row($result);
 		if($passwd==md5($_POST['password']) && $username==($_POST['username']))
-		{
-                    session_start();
-                    $_SESSION["aut"]=$aut;
-                    $_SESSION["user"]=$username;
-                    $cookie_name = $username;
-                    $a=getenv("REMOTE_ADDR"); 
-                    $cookie_value = $a;
-                    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-                    $_SESSION['userLogin'] = "Loggedin";
-                    echo '<meta http-equiv="refresh" content="0; URL=../view/menu.php">';                  
-        } 
-        else{	
+			{
+                    	    session_start();
+			    $_SESSION["aut"]=$aut;
+			    $_SESSION["user"]=$username;
+			    $cookie_name = $username;
+			    $a=getenv("REMOTE_ADDR"); 
+			    $cookie_value = $a;
+			    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+			    $_SESSION['userLogin'] = "Loggedin";
+			    echo '<meta http-equiv="refresh" content="0; URL=../view/menu.php">';                  
+			} 
+        	else
+		{	
                     echo "<br/>Hibás jelszó vagy felhasználónév";	
                     echo "<br/><a href=../view/>Vissza a bejelentkezéshez</a>";
 		}
-		
                 mysqli_close($con);
-        
-            }
-                
-        ?>
+            }  
+?>
