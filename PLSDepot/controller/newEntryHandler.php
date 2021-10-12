ed872171c7a8a3f1d0e5eb7a7ebecda0e56ea95f
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2021-10-12 20:51:07 +0200

    Végleges verzió

--- PLSDepot/controller/newEntryHandler.php
+++ PLSDepot/controller/newEntryHandler.php
@@ -2,46 +2,36 @@
     if(isset($_POST["newentry"]))  
     {
         require "database.php";
-        
-        
-        
         $dbname="pls";
         $con=connect("root","", $dbname);
         $query="select count(id) as total from cimzett where  nev = '".$_POST['cimzett_nev']."' and lakcim = '" .$_POST['cimzett_lakcim']."'";
         $res=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
         list($result)=mysqli_fetch_row($res);
-        if ($result == 0) {
+        if ($result == 0)
+        {
             $query="insert into cimzett(nev, lakcim, telefonszam, email) values ('".$_POST['cimzett_nev']."' ,'" .$_POST['cimzett_lakcim']."' ,'" .$_POST['cimzett_tel']."' ,'" .$_POST['cimzett_email']."')";
-                if(mysqli_query($con, $query))
+            if(mysqli_query($con, $query))
                         echo "Records added successfully.";
-                        else{
-                                echo "ERROR: Could not execute $query. " . mysqli_error($con);
-                            }
+            else
+                {
+                    echo "ERROR: Could not execute $query. " . mysqli_error($con);
+                }
         }
-        
         $query="insert into megbizas(megnevezes, suly, ar, feladas_datum, cimzettid ,partnercegid) values ('".$_POST['csomaginfo_megnevezes']."','".$_POST['csomaginfo_suly']."','".$_POST['csomaginfo_ar']."', CURRENT_TIMESTAMP,
         (select id from cimzett where  nev = '".$_POST['cimzett_nev']."' and lakcim = '" .$_POST['cimzett_lakcim']."'), (select id from partnercegek where  nev = '".$_POST['partnercegek_nev']."'))";
                 if(mysqli_query($con, $query))
-                        echo "Records added successfully.";
-                        else{
-                                echo "ERROR: Could not execute $query. " . mysqli_error($con);
-                            }
-
+                     echo "Records added successfully.";
+                else
+                    {
+                        echo "ERROR: Could not execute $query. " . mysqli_error($con);
+                    }
         $query="select max(id) from megbizas";
         $res=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
         list($id)=mysqli_fetch_row($res);
-        
         $query="INSERT INTO csomag_elokeszites VALUES(".$id.",0,1,0)";
         $res=mysqli_query($con,$query) or die ("Hiba: ".mysqli_error($con));
-
-
         mysqli_close($con);
         echo '<meta http-equiv="refresh" content="0; URL=../view/newentryform.php">';
-
-            
-    }
-       
-
-    
+    }   
 ?>
 
