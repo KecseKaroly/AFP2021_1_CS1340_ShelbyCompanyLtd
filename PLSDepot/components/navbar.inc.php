0b6a233b1b4fd9450c44a492c78bf9c1f8fff239
FeNToNelele <91262730+FeNToNelele@users.noreply.github.com>
2021-10-13 18:44:30 +0000

    Reszponzív navbar

--- PLSDepot/components/navbar.inc.php
+++ PLSDepot/components/navbar.inc.php
@@ -1,61 +1,45 @@
+<script src="../js/redirect.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/navbar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <head>
-
-  <script src="../js/redirect.js"></script>
-
-  <style>
-    .img {
-      padding: 20pt;
-      
+<img src="../img/PLS_logo.png" class="center" alt="" onclick="Redirect(1)" width="20%">
+<div class="topnav" id="myTopnav">
+  <?php
+    $tabcounter = 0;
+    $secondrow = false;
+    session_start();
+    $aut = $_SESSION['aut'];
+    if(isset($_SESSION['user']))
+    {
+        echo '<a href="../view/menu.php">Menü</a>';
+        echo '<a href="../view/listorders.php">Megbízások</a>';
     }
-  </style>
+    if($aut=="admin" || $aut=="felvivo")
+    {
+        echo '<a href="../view/newentryform.php">Új megbízás</a>';
+    }
+    if($aut=="admin")
+    {
+        echo '<a href="../view/profiles.php">Felhasználók kezelése</a>';
+    }
+    echo '<a href="../view/index.php" style="background-color: #cc4747; float: right;">Kijelentkezés</a>';
+  ?>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
 
-  <nav class="navbar navbar-fixed-top navbar-expand-lg bg-light">
-      
-    <div class="container-fluid">
-      <div class="col-4 text-center">
-        <img src="../img/PLS_logo.png" alt="" class="img-fluid" style="width: 80%; padding-left: 30pt;" onclick="Redirect(0)">
-      </div>
-      <div class="navbar">
-        <ul class="navbar-nav d-flex flex-row">
-          <div class="row">
-            <?php
-              $tabcounter = 0;
-              $secondrow = false;
-              session_start();
-              $aut = $_SESSION['aut'];
-              if(isset($_SESSION['user']))
-              {
-                
-                  echo '<div class="col-3">';
-                    echo '<img src="../img/megbizasok-menu.png" class="img-fluid" alt="Megbízások" onclick="Redirect(1)">';
-                  echo '</div>';
-                
-                $tabcounter++;
-              }
-              if($aut=="admin" || $aut=="felvivo") {
-                  echo '<div class="col-3">';
-                    echo '<img src="../img/uj_megbizas-menu.png" class="img-fluid" alt="Új megbízás" onclick="Redirect(2)">';
-                  echo '</div>';
-              }
-              if($aut=="admin")
-              {     
-                echo '<div class="col-3">';
-                  echo '<img src="../img/felhasznalok_kezelese-menu.png" class="img-fluid" alt="Felhasználók" onclick="Redirect(3)">';
-                echo '</div>';
-              }
-              if(isset($_SESSION['user']))
-              {
-              echo '<div class="col-1">';
-                echo '<img src="../img/kijelentkezes-menu.png" class="img-fluid" alt="Kijelentkezés" onclick="Redirect(4)">';
-              echo '</div>';
-              }
-            ?>
-          </div>
-        </ul>
-      </div>
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
 
-
-    </div>
-  </nav>
-
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
 </head>
